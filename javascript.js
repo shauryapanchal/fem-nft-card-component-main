918db0b15138093639ba7cb73fdcfc6b8f59a591
Shaurya Panchal <122795698+shauryapanchal@users.noreply.github.com>
2025-08-04 17:46:56 +0000

    Fixed the set opacity issue and cursor pointer for icon

--- javascript.js
+++ javascript.js
@@ -4,10 +4,12 @@ imageWrapper = document.querySelector('.image-wrapper');
 imageWrapper.addEventListener('mouseenter', function(){
     viewIcon.style.display = 'block';
     viewIcon.style.zIndex = '1';
+    nftImage.style.opacity = '0.5';
 });
 viewIcon.addEventListener('mouseenter', function(){
     nftImage.style.opacity = '0.5';
 });
 imageWrapper.addEventListener('mouseleave', function(){
     viewIcon.style.display = 'none';
+    nftImage.style.opacity = '1';
 });
